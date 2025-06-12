17703792c370c6635b761aa2742c5f0e4f781109
Borinees <joaoborin76@gmail.com>
2025-06-12 20:26:35 -0300

    Create funcion for API
    Creted the functions that use the API to get the information based on the cep and fill the form

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,16 +1,16 @@
-'use strict;' //Modo restrito
+'use strict'; //Modo restrito
 
 //API para formulario -> viacep.com.br
 
 //Função para limpar o formulario
 const limparFormulario = (endereco) => {
-    document.getElementById('cepText'),value = '';
-    document.getElementById('ruaText'),value = '';
-    document.getElementById('numeroText'),value = '';
-    document.getElementById('bairroText'),value = '';
-    document.getElementById('cidadeText'),value = '';
-    document.getElementById('estadoText'),value = '';
-    document.getElementById('contatoText'),value = '';
+    document.getElementById('cepText').value = '';
+    document.getElementById('ruaText').value = '';
+    document.getElementById('numeroText').value = '';
+    document.getElementById('bairroText').value = '';
+    document.getElementById('cidadeText').value = '';
+    document.getElementById('estadoText').value = '';
+    document.getElementById('contatoText').value = '';
 }
 
 //Função para preencher o formulario
@@ -25,4 +25,26 @@ const preencherFormulario = (endereco) => {
 const eNumero = (cepText) => /^[0-9]+$/.test(cepText);
 
 //Confere se o CEP tem o tamanho correto
-const cepValido = (cepText) => cepText.lenght == 8 && eNumero(cepText);
+const cepValido = (cepText) => cepText.length == 8 && eNumero(cepText);
+
+//Consumo de API viaCEP
+const pesquisarCep = async() => {
+    limparFormulario();
+    const cepInput = document.getElementById('cepText');
+    const url = `https://viacep.com.br/ws/${cepInput.value}/json/`;
+
+    if(cepValido(cepInput.value)) {
+        const dados = await fetch(url);
+        const adress = await dados.json();
+
+        if(adress.hasOwnProperty('erro')) {
+            alert('CEP não encontrado');
+        } else {
+            preencherFormulario(adress);
+        }
+    } else {
+        alert('CEP incorreto');
+    }
+}
+
+document.getElementById('cepText').addEventListener('focusout',pesquisarCep);
