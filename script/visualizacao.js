ad2eb52104dab27f20cb0b77217a1be32f61e36b
Borinees <joaoborin76@gmail.com>
2025-06-13 01:32:55 -0300

    Implement filter search
    Implemented a search bar to filter results

--- script/visualizacao.js
+++ script/visualizacao.js
@@ -1,21 +1,24 @@
 'use strict';
 
 const detalhes = document.getElementById('detalhes');
+const inputPesquisa = document.getElementById('pesquisar');
 
 //Pega os dados do localstorage
 const dadosSalvos = localStorage.getItem('listaNecessidade');
 const listaNecessidade = JSON.parse(dadosSalvos) || [];
 
+//Função para renderizar
+function renderizarLista(lista){
 
-if(listaNecessidade.length > 0) {
-    listaNecessidade.forEach((dados, index) => {
-        const div = document.createElement('div');
+    if(lista.length > 0) {
+        lista.forEach((dados) => {
+            const div = document.createElement('div');
         div.classList.add('card');
 
         div.innerHTML = `
-            <p><strong>Instituição:</strong> ${dados.instituicao}</p>
-            <p><strong>Tipo de ajuda:</strong> ${dados.tipoAjuda}</p>
-            <p><strong>Título:</strong> ${dados.titulo}</p>
+        <p><strong>Instituição:</strong> ${dados.instituicao}</p>
+        <p><strong>Tipo de ajuda:</strong> ${dados.tipoAjuda}</p>
+        <p><strong>Título:</strong> ${dados.titulo}</p>
             <p><strong>Descrição:</strong> ${dados.descricao}</p>
             <p><strong>CEP:</strong> ${dados.cep}</p>
             <p><strong>Rua:</strong> ${dados.rua}</p>
@@ -24,10 +27,27 @@ if(listaNecessidade.length > 0) {
             <p><strong>Cidade:</strong> ${dados.cidade}</p>
             <p><strong>Estado:</strong> ${dados.estado}</p>
             <p><strong>Contato:</strong> ${dados.contato}</p>
-        `;
-
-        detalhes.appendChild(div);
-    });
-} else {
-    detalhes.innerHTML = "<p>Nenhuma necessidade cadastrada ainda.</p>";
+            `;
+            
+            detalhes.appendChild(div);
+        });
+    } else {
+        detalhes.innerHTML = "<p>Nenhuma necessidade cadastrada ainda.</p>";
+    }
 }
+
+//Primeira exibição
+renderizarLista(listaNecessidade);
+
+//Função para filtrar
+inputPesquisa.addEventListener('input', () => {
+    const termo = inputPesquisa.value.toLowerCase();
+
+    const listaFiltrada = listaNecessidade.filter((item) =>
+        Object.values(item).some(valor =>
+            String(valor).toLowerCase().includes(termo)
+        )
+    );
+
+    renderizarLista(listaFiltrada);
+});
